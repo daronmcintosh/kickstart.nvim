e33a76a4e5aa2e3454047262d1026d29693bc45c
Daron McIntosh <mcintoshdaron@gmail.com>
2024-03-19 18:34:05 +0000

    configure filetree plugin for hidden files

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -7,7 +7,20 @@ return {
     'MunifTanjim/nui.nvim',
   },
   config = function()
-    require('neo-tree').setup {}
+    require('neo-tree').setup {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_by_name = {
+            '.git',
+            '.DS_Store',
+          },
+        },
+      },
+    }
 
     vim.keymap.set('n', '<leader>n', ':Neotree toggle filesystem right<CR>', { desc = 'Toggle filetree' })
   end,
