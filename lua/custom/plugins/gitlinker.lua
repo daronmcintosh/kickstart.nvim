b9f58c6fff4d1ba82da0c1a3f38aaf92ebdb9d96
Daron McIntosh <mcintoshdaron@gmail.com>
2024-03-27 15:39:53 +0000

    fix gitlinker and ssh copying

--- lua/custom/plugins/gitlinker.lua
+++ lua/custom/plugins/gitlinker.lua
@@ -1,4 +1,12 @@
 return {
   'ruifm/gitlinker.nvim',
-  opts = {},
+  opts = {
+    action_callback = function(url)
+      -- yank to unnamed register
+      vim.api.nvim_command('let @" = \'' .. url .. "'")
+      -- copy to the system clipboard using OSC52
+      -- vim.fn.OSCYankString(url)
+      vim.print(url)
+    end,
+  },
 }
