22f03e8b3805c5101dab6f8af734c1a0b03e67af
Daron McIntosh <mcintoshdaron@gmail.com>
2024-04-06 11:43:56 -0400

    add autoformatting and comment support for jsx/tsx

--- lua/plugins/comments.lua
+++ lua/plugins/comments.lua
@@ -1,8 +1,21 @@
 return {
   {
     'numToStr/Comment.nvim',
-    opts = {},
+    config = function()
+      require('Comment').setup {
+        pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+      }
+    end,
     lazy = false,
   },
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  {
+    'JoosepAlviste/nvim-ts-context-commentstring',
+    opts = {},
+    config = function()
+      require('ts_context_commentstring').setup {
+        enable_autocmd = false,
+      }
+    end,
+  },
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -130,6 +130,7 @@ return {
     'nvimtools/none-ls.nvim',
     config = function()
       local null_ls = require 'null-ls'
+      local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
       null_ls.setup {
         sources = {
           null_ls.builtins.code_actions.gitsigns,
@@ -138,6 +139,19 @@ return {
           null_ls.builtins.formatting.stylua,
           null_ls.builtins.hover.printenv,
         },
+        -- https://github.com/nvimtools/none-ls.nvim/wiki/Formatting-on-save#sync-formatting
+        on_attach = function(client, bufnr)
+          if client.supports_method 'textDocument/formatting' then
+            vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+            vim.api.nvim_create_autocmd('BufWritePre', {
+              group = augroup,
+              buffer = bufnr,
+              callback = function()
+                vim.lsp.buf.format { async = false }
+              end,
+            })
+          end
+        end,
       }
 
       -- TODO: update keymap to something more sensible
